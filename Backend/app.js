bb594d7161cc882461d08966eafa2dc76da9ce9b
KHUSH <patelkhush088@gmail.com>
2025-01-05 05:41:47 +0530

    first commit

--- Backend/app.js
+++ Backend/app.js
@@ -3,12 +3,19 @@ dotenv.config()
 const express = require("express")
 const cors = require('cors')
 const app = express()
+const connectTodb = require("./db/db")
+const userRoutes = require("./routes/user.routes")
+connectTodb()
 
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 
 app.get("/", (req, res) => {
     res.send("Hello khush")
 })
+
+app.use('./users', userRoutes)
 
 module.exports = app
